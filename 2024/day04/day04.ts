9fff6ebb89ba9df1eeb58684534071b8f59e2049
Kiri <56218513+kiriDevs@users.noreply.github.com>
2024-12-04 22:07:07 +0100

    [ts] 2024:04: Unify day04 parts

--- 2024/day04/day04.ts
+++ 2024/day04/day04.ts
@@ -6,12 +6,12 @@ const inputLines = (await Bun.file("input").text()).trim().split(EOL);
 type Grid = Array<Array<string>>;
 type Vector = [number, number]; // [x, y]
 
-const SEARCH_STRING = "MAS";
-const VALID_DIRECTIONS: Array<Vector> = [
-    [-1, -1], [1, -1], [-1, 1], [1, 1] // Diagonals
-];
+const STRAIGHT_DIRECTIONS: Array<Vector> = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+const DIAGONAL_DIRECTIONS: Array<Vector> = [[-1, -1], [1, -1], [-1, 1], [1, 1]]
 
-const HINT_SYMBOL = SEARCH_STRING.split("")[1];
+function VALID_DIRECTIONS(part2: boolean): Array<Vector> {
+    return part2 ? DIAGONAL_DIRECTIONS : STRAIGHT_DIRECTIONS.concat(DIAGONAL_DIRECTIONS);
+}
 
 function vecAdd(one: Vector, two: Vector): Vector {
     return [one[0] + two[0], one[1] + two[1]];
@@ -32,27 +32,27 @@ function isInBounds(point: Vector, grid: Grid) {
     )
 }
 
-function locateHints(grid: Grid): Array<Vector> {
+function locateHints(grid: Grid, hintSymbol: String): Array<Vector> {
     const hints: Array<Vector> = [];
     for (let y: number = 0; y < grid.length; y++) {
         const line = grid[y];
         for (let x: number = 0; x < line.length; x++) {
-            if (line[x] == HINT_SYMBOL) hints.push([x, y]);
+            if (line[x] == hintSymbol) hints.push([x, y]);
         }
     }
     return hints;
 }
 
-function investigateHint(hint: Vector): boolean {
+function investigateHint(hint: Vector, searchString: String, part2: boolean = false): number {
     let hitCount: number = 0;
-    for (let searchDirection of VALID_DIRECTIONS) {
+    for (let searchDirection of VALID_DIRECTIONS(part2)) {
         const directionString: Array<String> = [];
 
-        let searchLocation = vecAdd(hint, vecNeg(searchDirection));
+        let searchLocation = part2 ? vecAdd(hint, vecNeg(searchDirection)) : hint;
         let searchStringIndex = 0;
         while (
             isInBounds(searchLocation, inputGrid)
-            && searchStringIndex < SEARCH_STRING.length
+            && searchStringIndex < searchString.length
         ) {
             // TODO: Maybe fail the direction on first wrong letter?
             
@@ -60,12 +60,25 @@ function investigateHint(hint: Vector): boolean {
             searchLocation = vecAdd(searchLocation, searchDirection);
             searchStringIndex += 1;
         }
-        if (directionString.join("") == SEARCH_STRING) hitCount += 1;
+        if (directionString.join("") == searchString) hitCount += 1;
     }
-    return hitCount == 2;
+    if (part2) return Number(hitCount == 2);
+    else return hitCount;
 }
 
 const inputGrid: Grid = inputLines.map((inputLine: string) => inputLine.split(""));
-const hints: Array<Vector> = locateHints(inputGrid);
-const totalHitCount = hints.map(investigateHint).reduce((acc, didHit) => didHit ? acc + 1 : acc, 0);
-console.log("Part 2:", totalHitCount);
+
+function part1() {
+    const hints: Array<Vector> = locateHints(inputGrid, "X");
+    const hitCounts: Array<number>= hints.map((hint) => investigateHint(hint, "XMAS"));
+    return hitCounts.reduce((acc, hitCount) => acc + hitCount);
+}
+
+function part2() {
+    const hints: Array<Vector> = locateHints(inputGrid, "A");
+    const hitCounts: Array<number> = hints.map((hint) => investigateHint(hint, "MAS", true));
+    return hitCounts.reduce((acc, hitCount) => acc + hitCount);
+}
+
+console.log("Part 1:", part1());
+console.log("Part 2:", part2());
